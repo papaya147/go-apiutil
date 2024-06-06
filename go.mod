54c9b303b8198aeac47e2f13f407fe1465852fd9
Abhinav Srivatsa <asrivatsa6@gmail.com>
2024-06-06 12:09:16 +0530

    token patches

--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 
 require (
 	aidanwoods.dev/go-result v0.1.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
--- token.go
+++ token.go
@@ -2,11 +2,19 @@ package apiutil
 
 import (
 	"aidanwoods.dev/go-paseto"
+	"context"
+	"encoding/gob"
 	"encoding/json"
 	"errors"
 	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/gorilla/sessions"
+	"net/http"
 	"time"
 )
+
+func init() {
+	gob.Register(TokenType(0))
+}
 
 type TokenType int
 
@@ -22,13 +30,23 @@ type TokenPayload interface {
 type TokenMaker[P TokenPayload] interface {
 	CreateToken(P, TokenType) (string, P, error)
 	VerifyToken(string, TokenType) (P, error)
+	Middleware(*sessions.CookieStore, string, TokenType) func(http.Handler) http.Handler
+	TokenFromRequest(*http.Request, TokenType) (P, error)
 }
 
 type PasetoMaker[P TokenPayload] struct {
-	tokenDuration                        map[TokenType]time.Duration
-	secretKey                            paseto.V4SymmetricKey
-	parser                               paseto.Parser
-	invalidTokenError, expiredTokenError error
+	tokenDuration                                      map[TokenType]time.Duration
+	secretKey                                          paseto.V4SymmetricKey
+	parser                                             paseto.Parser
+	errInvalidToken, errExpiredToken, errInvalidCookie error
+}
+
+func (p PasetoMaker[P]) Middleware(store *sessions.CookieStore, sessionName string, tokenType TokenType) func(http.Handler) http.Handler {
+	return tokenMiddleware[P](p, store, sessionName, tokenType, p.errInvalidCookie)
+}
+
+func (p PasetoMaker[P]) TokenFromRequest(request *http.Request, tokenType TokenType) (P, error) {
+	return tokenFromRequest[P](request, tokenType, p.errInvalidCookie)
 }
 
 func (p PasetoMaker[P]) CreateToken(payload P, tokenType TokenType) (string, P, error) {
@@ -58,37 +76,38 @@ func (p PasetoMaker[P]) VerifyToken(s string, tokenType TokenType) (P, error) {
 	decrypted, err := p.parser.ParseV4Local(p.secretKey, s, nil)
 	if err != nil {
 		if err.Error() == "this token has expired" {
-			return payload, p.expiredTokenError
+			return payload, p.errExpiredToken
 		}
-		return payload, p.invalidTokenError
+		return payload, p.errInvalidToken
 	}
 
 	j, err := decrypted.GetString("payload")
 	if err != nil {
-		return payload, p.invalidTokenError
+		return payload, p.errInvalidToken
 	}
 
 	if json.Unmarshal([]byte(j), &payload) != nil {
-		return payload, p.invalidTokenError
+		return payload, p.errInvalidToken
 	}
 
 	if payload.GetType() != tokenType {
-		return payload, p.invalidTokenError
+		return payload, p.errInvalidToken
 	}
 
 	return payload, nil
 }
 
-func NewPasetoMaker[P TokenPayload](durations map[TokenType]time.Duration, invalidTokenError, expiredTokenError error) TokenMaker[P] {
+func NewPasetoMaker[P TokenPayload](durations map[TokenType]time.Duration, errInvalidToken, errExpiredToken, errInvalidCookie error) TokenMaker[P] {
 	parser := paseto.NewParser()
 	parser.AddRule(paseto.NotExpired(), paseto.NotBeforeNbf())
 
 	return &PasetoMaker[P]{
-		tokenDuration:     durations,
-		secretKey:         paseto.NewV4SymmetricKey(),
-		parser:            parser,
-		invalidTokenError: invalidTokenError,
-		expiredTokenError: expiredTokenError,
+		tokenDuration:    durations,
+		secretKey:        paseto.NewV4SymmetricKey(),
+		parser:           parser,
+		errInvalidToken:  errInvalidToken,
+		errExpiredToken:  errExpiredToken,
+		errInvalidCookie: errInvalidCookie,
 	}
 }
 
@@ -124,11 +143,11 @@ func (j JWTPayload) GetAudience() (jwt.ClaimStrings, error) {
 }
 
 type JWTMaker[P TokenPayload] struct {
-	tokenDuration                        map[TokenType]time.Duration
-	secretKey                            []byte
-	signingMethod                        jwt.SigningMethod
-	parser                               *jwt.Parser
-	invalidTokenError, expiredTokenError error
+	tokenDuration                                      map[TokenType]time.Duration
+	secretKey                                          []byte
+	signingMethod                                      jwt.SigningMethod
+	parser                                             *jwt.Parser
+	errInvalidToken, errExpiredToken, errInvalidCookie error
 }
 
 func (j JWTMaker[P]) CreateToken(payload P, tokenType TokenType) (string, P, error) {
@@ -161,20 +180,20 @@ func (j JWTMaker[P]) VerifyToken(s string, tokenType TokenType) (P, error) {
 	var payload P
 	token, err := j.parser.Parse(s, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, j.invalidTokenError
+			return nil, j.errInvalidToken
 		}
 		return j.secretKey, nil
 	})
 	if err != nil || !token.Valid {
 		if errors.Is(err, jwt.ErrTokenInvalidClaims) {
-			return payload, j.expiredTokenError
+			return payload, j.errExpiredToken
 		}
 		return payload, err
 	}
 
 	tokenPayload, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		return payload, j.invalidTokenError
+		return payload, j.errInvalidToken
 	}
 
 	if err := json.Unmarshal([]byte(tokenPayload["payload"].(string)), &payload); err != nil {
@@ -182,13 +201,21 @@ func (j JWTMaker[P]) VerifyToken(s string, tokenType TokenType) (P, error) {
 	}
 
 	if payload.GetType() != tokenType {
-		return payload, j.invalidTokenError
+		return payload, j.errInvalidToken
 	}
 
 	return payload, nil
 }
 
-func NewJWTMaker[P TokenPayload](durations map[TokenType]time.Duration, invalidTokenError, expiredTokenError error, secret string) TokenMaker[P] {
+func (j JWTMaker[P]) Middleware(store *sessions.CookieStore, sessionName string, tokenType TokenType) func(http.Handler) http.Handler {
+	return tokenMiddleware[P](j, store, sessionName, tokenType, j.errInvalidCookie)
+}
+
+func (j JWTMaker[P]) TokenFromRequest(request *http.Request, tokenType TokenType) (P, error) {
+	return tokenFromRequest[P](request, tokenType, j.errInvalidCookie)
+}
+
+func NewJWTMaker[P TokenPayload](durations map[TokenType]time.Duration, errInvalidToken, errExpiredToken, errInvalidCookie error, secret string) TokenMaker[P] {
 	signingMethod := jwt.SigningMethodHS256
 
 	return &JWTMaker[P]{
@@ -199,7 +226,44 @@ func NewJWTMaker[P TokenPayload](durations map[TokenType]time.Duration, invalidT
 			jwt.WithValidMethods([]string{signingMethod.Name}),
 			jwt.WithExpirationRequired(),
 		),
-		invalidTokenError: invalidTokenError,
-		expiredTokenError: expiredTokenError,
+		errInvalidToken:  errInvalidToken,
+		errExpiredToken:  errExpiredToken,
+		errInvalidCookie: errInvalidCookie,
 	}
+}
+
+func tokenMiddleware[P TokenPayload](tokenMaker TokenMaker[P], cookieStore *sessions.CookieStore, sessionName string, tokenType TokenType, errInvalidCookie error) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			sess, _ := cookieStore.Get(r, sessionName)
+			token, ok := sess.Values[tokenType].(string)
+			if !ok {
+				ErrorJson(w, errInvalidCookie)
+				return
+			}
+
+			payload, err := tokenMaker.VerifyToken(token, tokenType)
+			if err != nil {
+				ErrorJson(w, err)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, tokenType, payload)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func tokenFromRequest[P TokenPayload](r *http.Request, tokenType TokenType, errInvalidToken error) (P, error) {
+	ctx := r.Context()
+	var payload P
+
+	payload, ok := ctx.Value(tokenType).(P)
+	if !ok {
+		return payload, errInvalidToken
+	}
+
+	return payload, nil
 }
